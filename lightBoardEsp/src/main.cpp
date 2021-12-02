cd5c257abd01d5f911d3be73139ab11cd4b5d509
k <kkraczyna@gmail.com>
2021-12-02 12:16:26 +0100

    fuck SPI, uart is only one true love

--- lightBoardEsp/src/main.cpp
+++ lightBoardEsp/src/main.cpp
@@ -1,22 +1,47 @@
-#include "SPI.h"
+#include "uart.hpp"
 char buff[]="Hello Slave\n";
 
 void setup() {
   Serial.begin(115200);
   Serial.println("siemanero, tu ESP!");
-  SPI.begin();
+  UART_init();
 }
 
 void loop() {
-  Serial.println("sending stuff via SPI");
-  // char recvBuff[32] = {0};
-  for(int i=0; i < sizeof buff; i++) {
-    char a = SPI.transfer(buff[i]);
-    // recvBuff[i] = a;
-    Serial.print(a);
-  }
-  Serial.print("\n");
-  // recvBuff[sizeof buff] = '\0';
-  // Serial.println(recvBuff);
-  delay(1000);  
+  UART_update();
+  
+  Serial.print("inputState: ");
+  Serial.println(PIC::inputState);
+  Serial.print("outputState: ");
+  Serial.println(PIC::outputState);
+  Serial.print("dimmer_1: ");
+  Serial.println(PIC::dimmer_1);
+  Serial.print("dimmer_2: ");
+  Serial.println(PIC::dimmer_2);
+  Serial.print("dimmer_3: ");
+  Serial.println(PIC::dimmer_3);
+  Serial.print("dimmer_4: ");
+  Serial.println(PIC::dimmer_4);
+  Serial.print("dimmer_5: ");
+  Serial.println(PIC::dimmer_5);
+  Serial.print("dimmer_6: ");
+  Serial.println(PIC::dimmer_6);
+  Serial.print("dimmer_7: ");
+  Serial.println(PIC::dimmer_7);
+  Serial.print("current_1: ");
+  Serial.println(PIC::current_1);
+  Serial.print("current_2: ");
+  Serial.println(PIC::current_2);
+  Serial.print("current_3: ");
+  Serial.println(PIC::current_3);
+  Serial.print("current_4: ");
+  Serial.println(PIC::current_4);
+  Serial.print("current_5: ");
+  Serial.println(PIC::current_5);
+  Serial.print("current_6: ");
+  Serial.println(PIC::current_6);
+  Serial.print("current_7: ");
+  Serial.println(PIC::current_7);
+
+  delay(10);  
 }
---
+++ lightBoardEsp/src/spiFrames.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <stdint.h>
+
+#define SPI_SOF 0xAA
+#define SPI_EOF 0xBB
+#define C1  0xCC
+#define C2  0xDD
+#define C3  0xEE
+
+// PIC -> ESP
+#define COMMAND_DEVICE_STATE 0x10
+
+// ESP -> PIC
+#define COMMAND_SET_OUTPUT 0x50
+#define COMMAND_SET_DIMMER 0x51
+
+#pragma pack(push, 1)
+struct OutputBufferFrame {
+    uint8_t sof;
+    uint8_t com;
+    uint8_t d1;
+    uint8_t c1;
+    uint8_t d2;
+    uint8_t d3;
+    uint8_t c2;
+    uint8_t d4;
+    uint16_t crc;
+    uint8_t eof;
+};
+#pragma pack(pop)
+
+union OutputBuffer {
+    struct OutputBufferFrame frame;
+    char data[sizeof (struct OutputBufferFrame)];
+};
+
+union OutputBuffer outputBuffer;
+
+#pragma pack(push, 1)
+struct InputBufferFrame {
+    uint8_t sof;
+    uint8_t com;
+    uint8_t inputState;
+    uint8_t outputState;
+    uint8_t c1;
+    uint8_t dimmer_1;
+    uint8_t dimmer_2;
+    uint8_t dimmer_3;
+    uint8_t dimmer_4;
+    uint8_t dimmer_5;
+    uint8_t dimmer_6;
+    uint8_t dimmer_7;
+    uint8_t c2;
+    uint16_t current_1;
+    uint16_t current_2;
+    uint16_t current_3;
+    uint16_t current_4;
+    uint16_t current_5;
+    uint16_t current_6;
+    uint16_t current_7;
+    uint8_t c3;
+    uint16_t crc;
+    uint8_t eof;
+};
+#pragma pack(pop)
+
+union InputBuffer {
+    struct InputBufferFrame frame;
+    char data[sizeof (struct InputBufferFrame)];
+};
+
+
+union InputBuffer inputBuffer;
+
+int a = sizeof (struct InputBufferFrame);
